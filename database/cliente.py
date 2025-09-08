f0f46b4e11d9c65129a435dcf538c1f1d2cf4abc
CaduSaless <cadubss2@gmail.com>
2025-09-08 04:16:26 -0300

    First commit

---
+++ database/cliente.py
@@ -0,0 +1,7 @@
+CLIENTES = [
+    {"id": 1, "nome": "Guilherme", "email": "gui@gmail.com"},
+    {"id": 2, "nome": "Vinicius", "email": "vini@gmail.com"},
+    {"id": 3, "nome": "Heitor", "email": "heitor@gmail.com"},
+    {"id": 4, "nome": "Ian", "email": "ian@gmail.com"},
+    {"id": 5, "nome": "Yan", "email": "yan@gmail.com"}
+]
---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from routes.home import home_route
+from routes.cliente import cliente_route
+
+app = Flask(__name__)
+
+app.register_blueprint(home_route)
+
+app.run(debug=True, port=80)
